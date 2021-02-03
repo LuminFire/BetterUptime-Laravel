410b3d452053decbd9293e0f38e14d4628d2a453
Andrew Minion <andrew.minion@luminfire.com>
2021-02-03 15:47:21 -0600

    add tests for default config values

--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -2,11 +2,30 @@
 
 namespace BrilliantPackages\BetterUptimeLaravel\Tests;
 
+use Illuminate\Console\Scheduling\Schedule;
+
 class ConfigTest extends TestCase
 {
     /** @test */
     public function telescope_ignores_better_uptime_when_enabled()
     {
         $this->assertContains('better-uptime', config('telescope.ignore_paths'));
+    }
+
+    /** @test */
+    public function default_route_exists()
+    {
+        $this->assertEquals('better-uptime', $this->app['router']->getRoutes()->getByName('betteruptime')->uri);
+    }
+
+    /** @test */
+    public function default_schedule_works()
+    {
+        $scheduler = $this->app->make(Schedule::class);
+
+        $events = $scheduler->events();
+
+        $this->assertStringContainsString('better-uptime:ping', $events[0]->command);
+        $this->assertEquals('*/5 * * * *', $events[0]->expression);
     }
 }
