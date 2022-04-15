f9635d46a6fb7e41af3c9fd88da1462d612b0685
Andrew Minion <andrew.minion@luminfire.com>
2022-04-15 14:37:27 -0500

    improve error handling

--- src/Commands/PingBetterUptime.php
+++ src/Commands/PingBetterUptime.php
@@ -3,6 +3,7 @@
 namespace BrilliantPackages\BetterUptimeLaravel\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Http\Client\RequestException;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 
@@ -49,21 +50,31 @@ class PingBetterUptime extends Command
             return self::FAILURE;
         }
 
-        $response = Http::retry(config('betteruptime-laravel.heartbeat.retry.count'), config('betteruptime-laravel.heartbeat.retry.delay'))->head(config('betteruptime-laravel.heartbeat.url'));
-        if ($response->successful()) {
-            $this->info('Status ' . $response->getStatusCode());
-        } else {
-            $errorString = 'Error code ' . $response->status();
+        try {
+            $response = Http::retry(config('betteruptime-laravel.heartbeat.retry.count'), config('betteruptime-laravel.heartbeat.retry.delay'))->head(config('betteruptime-laravel.heartbeat.url'));
+            if ($response->successful()) {
+                $this->info('Status ' . $response->getStatusCode());
+            } else {
+                $errorString = 'Error code ' . $response->status();
+                if (! empty($response->body())) {
+                    $errorString .= ' with message ' . $response->body();
+                };
 
-            if (! empty($response->body())) {
-                $errorString .= ' with message ' . $response->body();
-            };
+                $this->error($errorString);
+                Log::warning('PingBetterUptime failed; ' . $errorString);
+                return self::FAILURE;
+            }
+
+            return self::SUCCESS;
+        } catch (RequestException $e) {
+            $errorString = 'Error code ' . $e->getCode();
+            if ($e->getMessage() && 'HTTP request returned status code ' . $e->getCode() !== $e->getMessage()) {
+                $errorString .= ' with message ' . $e->getMessage();
+            }
 
             $this->error($errorString);
-            Log::warning('PingBetterUptime failed; ' . $errorString);
+            Log::warning('PingBetterUptime failed; ' . $e->getMessage());
             return self::FAILURE;
         }
-
-        return self::SUCCESS;
     }
 }
